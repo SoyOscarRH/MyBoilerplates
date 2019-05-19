868df32c9e9d7e0e639563b36ba5a315b3d5b0ff
Oscar Rosas <soyoscarrh@gmail.com>
2019-05-19 18:14:46 -0500

    Update with eslint

--- Code/App/index.tsx
+++ Code/App/index.tsx
@@ -2,20 +2,16 @@ import React, { FunctionComponent } from "react"
 import ReactDOM from "react-dom"
 
 const App: FunctionComponent = () => (
-    <main>
-        <br />
-
-        Hi World 
-        
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-
-    </main>
+  <main>
+    <br />
+    Hi World
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+  </main>
 )
-
 
 ReactDOM.render(<App />, document.getElementById("ReactApp"))
